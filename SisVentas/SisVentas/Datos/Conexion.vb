7f3cfb22fca6d45d6469ed7a69c37257d85b2141
IdEclipse <IdEclipse@hotmail.com>
2020-04-01 01:44:00 -0500

    Cadena de conexion creada por espacio My

---
+++ SisVentas/SisVentas/Datos/Conexion.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class Conexion
+    Private conexion As New SqlConnection(My.Settings.cnx)
+
+
+    Sub Conectar()
+        If conexion.State = ConnectionState.Closed Then
+            conexion.Open()
+        End If
+    End Sub
+
+    Sub Desconectar()
+        If conexion.State = ConnectionState.Open Then
+            conexion.Close()
+        End If
+
+    End Sub
+
+End Class
